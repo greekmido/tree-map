f5a78c1ee0c02985cb8f38c4a68408c1608a1c3d
mohammad alameri <onex911@hotmail.com>
2022-04-07 16:33:48 +0200

    complete tree without titles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './index.css';
 import * as d3 from 'd3';
-
+import {TextBox} from 'd3plus-text'
 let height = 700;
-let width =900;
+let width =1200;
 let margin = {"top":20,"bottom":20,"left":50,"right":50};  
 let svg = d3.select("body").append("svg").attr("width",width).attr("height",height);
 let colorpallet=[...d3.schemeTableau10,...d3.schemeSet2,"#01578c"]
@@ -11,7 +11,10 @@ Promise.all([d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tr
              d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json"),
             d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json")])
             .then((data)=>{mapIt(data[0])}).catch((err)=>console.error(err));
-
+let myTextBox = new TextBox()
+function wrapIt(text){
+  myTextBox.data([{text:text}])
+}
 
 function ready(games,movies,kickstarter){
 
@@ -19,19 +22,29 @@ function ready(games,movies,kickstarter){
 function mapIt(data){
     let root = d3.hierarchy(data);
     let layout = d3.treemap();
-    layout.size([width-margin.right-margin.left, height-margin.top-margin.bottom])
+    layout.size([width-margin.right-margin.left, height-margin.top-margin.bottom]).padding(0.5)
     root.sum(function(d) {
         return d.value;
       });
-    layout(root);
-    let catList = root.ancestors().map((cat)=>{if(cat.data.name){return cat.data.name}});
-      console.log(catList)
-    //colorScale.domain([])
-    svg.append("g").selectAll("rect").data(root.leaves()).enter().append("rect")
+    layout(root.sort((a,b)=>d3.descending(a.value,b.value)));
+    document.getElementById("title").innerHTML=root.descendants()[0].data.name
+    let catList=[] ;
+    root.leaves().forEach((leaf)=>{
+      if(leaf.parent.data.name){
+        if (!catList.includes(leaf.parent.data.name)){
+          catList.push(leaf.parent.data.name);}}});
+    colorScale.domain(catList);
+    
+    let myG = svg.append("g").attr("transform",`translate(${margin.right},${margin.top})`).selectAll("g").data(root.leaves()).enter().append("g");
+    myG.append("rect")
     .attr("x",(d)=>d.x0).attr("y",(d)=>d.y0)
     .attr("width",(d)=>d.x1-d.x0).attr("height",(d)=>d.y1-d.y0)
-    .attr("fill","black").style("opacity","0.2").attr("stroke","black")
-}
+    .attr("fill",(d)=>colorScale(d.data.category)).attr("id",(d)=>d.data.name)
+    myG.append("text").html((d)=>{
+      wrapIt(d.data.name)
+    })
+    .attr("x",(d)=>d.x0).attr("y",(d)=>d.y0).attr("width",(d)=>d.x1-d.x2).attr("height",(d)=>d.y1-d.y0);
+  }
 
 
 
